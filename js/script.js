6c9c283d8c39ccbbead6573b18d32cbd5ddaaf38
alifrontend96 <alifrontend96@gmail.com>
2018-07-16 21:27:48 +0530

    Intersection Ovsrever changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-$(window).scroll(function () {
-    $('img').each(function () {
-        if ($(this).offset().top < ($(window).scrollTop() + $(window).height())) {
-            var dataAttr = $(this).data('src');
-            $(this).attr('src', dataAttr);
-            $(this).removeAttr('data-src');
+// $(window).scroll(function () {
+//     $('img').each(function () {
+//         if ($(this).offset().top < ($(window).scrollTop() + $(window).height())) {
+//             let dataAttr = $(this).data('src');
+//             $(this).attr('src', dataAttr);
+//             $(this).removeAttr('data-src');
+//         }
+//     });
+// })
+
+const observer = new IntersectionObserver((entries, options) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting == true) {
+            let dataAttr = entry.target.firstElementChild.getAttribute('data-src');
+            if (dataAttr !== null) {
+                entry.target.firstElementChild.setAttribute('src', dataAttr);
+                entry.target.firstElementChild.removeAttribute('data-src');
+            }
         }
     });
-})
+
+});
+
+let items = document.querySelectorAll('.flex > .inner');
+items.forEach((item) => {
+    observer.observe(item);
+});
+
