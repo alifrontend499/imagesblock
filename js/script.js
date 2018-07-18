9ce72a95df211a9affdbc81e1169008e8986f7bd
Alim499 <ali@multichannelcreative.co.uk>
2018-07-18 10:40:40 +0530

    some changes

--- js/script.js
+++ js/script.js
@@ -1,34 +1,37 @@
 // $(window).scroll(function () {    
 //     $('img').each(function () {
 //         if ($(this).attr('data-src') && $(this).offset().top < ($(window).scrollTop() + $(window).height()) + 200) {
-//             let dataAttr = $(this).data('src');
+//             var dataAttr = $(this).data('src');
 //             $(this).attr('src', dataAttr);
 //             $(this).removeAttr('data-src');
 //         }
 //     });
 // });
-
-
-let myOptions = {
-    root: null,
-    rootMargin: "50% 0px 0px 0px",
-    threshold: 0
-};
-let callBackFunc = (entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            let dataAttr = entry.target.firstElementChild.getAttribute('data-src');
-            if (dataAttr !== null) {
-                entry.target.firstElementChild.setAttribute('src', dataAttr);
-                entry.target.firstElementChild.removeAttribute('data-src');
+(function() {
+    "use strict";
+    var myOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0
+    };
+    var callBackFunc = function(entries) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                var dataAttr = entry.target.firstElementChild.getAttribute('data-src');
+                if (dataAttr !== null) {
+                    entry.target.firstElementChild.setAttribute('src', dataAttr);
+                    entry.target.firstElementChild.removeAttribute('data-src');
+                }
             }
-        }
-    });
-}
-let observer = new IntersectionObserver(callBackFunc, myOptions);
+        });
+    }
+    var observer = new IntersectionObserver(callBackFunc, myOptions);
 
-let items = document.querySelectorAll('.flex > .inner');
-
-items.forEach((item) => {
-    observer.observe(item);
-});
+    var items = document.querySelectorAll('.flex > .inner');
+    for (var i = 0; i < items.length; i++) {
+        observer.observe(items[i]); 
+    }
+    // items.forEach(function(item) {
+    //     observer.observe(item);
+    // });
+})();
